299287cc34e036bc57fd2833a289c21025ea92c5
VinitGrover <vinitgrover255@gmail.com>
2022-05-22 13:55:54 +0530

    Adding uc1

--- LineProblem.java
+++ LineProblem.java
@@ -2,6 +2,8 @@ package com.bridgelabz;
 public class LineProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
+
+        System.out.println("The player starts from position zero");
     }
 }
 
