37f64accf4d1b50fbea08d764f13c0ed50741539
VinitGrover <104669938+VinitGrover@users.noreply.github.com>
2022-06-16 21:20:52 +0530

    Update and rename LineProblem.java to SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Random;
 
 
-public class LineProblem {
+public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
         System.out.println("The player starts from position zero");
